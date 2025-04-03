5f334304eb697735d2d2ff17bd4f83e296d60348
Arukast <altatubagus@gmail.comgit config --global user.email altatubagus@gmail.comgit config --global user.email altatubagus@gmail.com>
2025-04-03 17:55:28 +0700

    rev soal2

--- soal2.js
+++ soal2.js
@@ -1,18 +1,22 @@
+// deklarasi variable
 let word1, word2, word3, word4, word5;
-
+// pemberian value setiap variable
 word1 = "Angsa";
 word2 = "KataK";
 word3 = "kasur empuk";
 word4 = "Aku Suka Kamu";
 word5 = "Ibu Ratna antar ubi.";
 
+// deklarasi fungsi untuk mencari kata palindrom dengan parameter berupa string
 function palindrom(wordInput){
+    // melakukan RegEx untuk menghapus semua karakter selain alphabet dan merubah string menjadi lower case 
     const word = wordInput.replace(/[^a-zA-Z]/g, '').toLowerCase();
-    const reverseFixWord = word.split("").reverse().join("");
-
-    return word === reverseFixWord ? "eureeka!" : "suka blyat";
+    // merubah kata yang telah diubah sebelumnya dengan cara mengubahnya menjadi array lalu di reverse dan setelah itu diubah kembali menjadi string
+    const reverseWord = word.split("").reverse().join("");
+    // melakukan conditional operator dengan mengecek apakah variable word dan reverseWord sama baik dalam value dan tipe data 
+    return word === reverseWord ? "eureeka!" : "suka blyat";
 }
-
+// menampilkan output
 console.log(palindrom(word1));
 console.log(palindrom(word2));
 console.log(palindrom(word3));
